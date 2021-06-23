5b346888d13156081fe43a0a2f5437f6f0e64077
zubair <zubairashraf403@gmail.com>
2021-06-24 01:20:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Quote from "./components/Quote";
+
+import React, { useState, useEffect } from "react";
 
 function App() {
+  //state
+  const [quote, setQuote] = useState({
+    anime: null,
+    character: null,
+    quote: null,
+  });
+
+  const generate = async () => {
+    setQuote(await fetchQuote());
+  };
+  const fetchQuote = async () => {
+    return await fetch("https://animechan.vercel.app/api/random").then(
+      (response) => response.json()
+    );
+  };
+
+  useEffect(async () => {
+    setQuote(await fetchQuote());
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Quote quote={quote} />
+
+      <button onClick={generate}>Generate new Quote</button>
     </div>
   );
 }
